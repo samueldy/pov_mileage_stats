a119f54d40be890a028f29029f3deb500ddce6cd
Sam <samueldy@umich.edu>
2018-11-10 19:59:23 -0500

    /load_data: Auto stash before rebase of "master"

--- pov_mileage_stats/calculate_statistics.py
+++ pov_mileage_stats/calculate_statistics.py
@@ -1,7 +1,27 @@
-# -*- coding: utf-8 -*-
+# --- coding: utf-8 ---
 """
 calculateStatistics.py
 A small command-line tool to calculate mileage statistics for a personally-owned vehicle.
 
 Handles statistics calculation.
 """
+
+import pandas as pd
+
+def calculate_basic_stats(data):
+    """
+    Performs basic statistics calculations for car data.
+
+    Arguments:
+    ----------
+    data: a DataFrame containing at least two columns: a 'Date' column and a 'Miles' column.
+
+    Returns:
+    -------=
+    results: a dict containing some basic statistics:
+                mean miles driven
+                median miles driven
+
+    """
+    # First, convert all NAs to zeros.
+    
